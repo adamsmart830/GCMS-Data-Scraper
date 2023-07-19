a642e94dde940912c7918d19f9162d41c92a0c9e
Adam <123777013+adamsmart830@users.noreply.github.com>
2023-07-19 10:36:49 -0400

    Version 0.1 kmw-B

--- kmw-A.py
+++ kmw-A.py
@@ -10,7 +10,7 @@ class Sample:
     def __init__(self, _pc, _sn, _rt):
         self.pc = _pc
         self.sn = _sn
-        self.rt = _rt
+        self.rt = float(_rt)
 
     def getPC(self):
         return self.pc
@@ -66,17 +66,17 @@ def kmw_A(dict, keySort):
             list_val = ['', smpl.pc, smpl.sn, smpl.rt]
             row_list.append(list_val)
 
-    with open('../output/kmw-data-scrape.csv', 'w', newline='') as file:
+    with open('../output/kmw-data-scrape-A.csv', 'w', newline='') as file:
         writer = csv.writer(file)
         writer.writerows(row_list)
 
     file.close()
 
     # Reading the csv file
-    df_new = pd.read_csv('../output/kmw-data-scrape.csv')
+    df_new = pd.read_csv('../output/kmw-data-scrape-A.csv')
     
     # saving xlsx file
-    GFG = pd.ExcelWriter('../output/kmw-data-scrape.xlsx')
+    GFG = pd.ExcelWriter('../output/kmw-data-scrape-A.xlsx')
     df_new.to_excel(GFG, index=False)
     
     GFG._save()
@@ -86,13 +86,56 @@ def kmw_A(dict, keySort):
 def kmw_B(dict, keySort, sample_array):
     rt_dict = {}
     getSTDevDict(dict, keySort, rt_dict)
-    parseData(rt_dict, sample_array)
+    new_rt_dict = {}
+    parseData(rt_dict, new_rt_dict, sample_array)
+    
+    outputB(rt_dict, new_rt_dict)
 
-
-def parseData(rt_dict, sample_array):
+def outputB(rt_dict, new_rt_dict):
     rt_dict = dict(sorted(rt_dict.items(), key=lambda item:item[0]))
     keysList = list(rt_dict.keys())
-    print(keysList)
+    row_list = [["Retention Time", "Standard Deviation", "Proposed Compound", "Sample", "Actual Retention Time"]]
+    
+    for key in keysList:
+        list_val = [key, rt_dict[key], "", "", ""]
+        row_list.append(list_val)
+        for sample in new_rt_dict[key]:
+            list_val = ["", "", sample.pc, sample.sn, sample.rt]
+            row_list.append(list_val)
+        list_val = ["", "","","",""]
+        row_list.append(list_val)
+        
+    with open('../output/kmw-data-scrape-B.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(row_list)
+
+    file.close()
+
+    # Reading the csv file
+    df_new = pd.read_csv('../output/kmw-data-scrape-B.csv')
+    
+    # saving xlsx file
+    GFG = pd.ExcelWriter('../output/kmw-data-scrape-B.xlsx')
+    df_new.to_excel(GFG, index=False)
+    
+    GFG._save()
+
+    #print(row_list)  print("Hello World")
+
+
+def parseData(rt_dict, new_rt_dict, sample_array):
+    rt_dict = dict(sorted(rt_dict.items(), key=lambda item:item[0]))
+    keysList = list(rt_dict.keys())
+    for key in keysList:
+        stdev = rt_dict[key]
+        for sample in sample_array:
+            if((key + stdev) > sample.rt and (key - stdev) < sample.rt):
+                if(new_rt_dict.get(key) == None):
+                    new_rt_dict[key] = []
+                new_rt_dict[key].append(sample)
+        
+    #print(new_rt_dict)
+
 
         
 
@@ -122,7 +165,7 @@ def getSTDevDict(dict, keySort, rt_dict):
         std_dev = max(abs_rt_h, abs_rt_l, 0.01)
         rt_dict[avg_rt] = std_dev
 
-    print(rt_dict)
+    #print(rt_dict)
 
 
 def main():
@@ -148,6 +191,9 @@ def main():
     if(ui == 'a'):
         kmw_A(dict, keySort)
     elif(ui == 'b'):
+        kmw_B(dict, keySort, sample_array)
+    else:
+        kmw_A(dict, keySort)
         kmw_B(dict, keySort, sample_array)
 
 
